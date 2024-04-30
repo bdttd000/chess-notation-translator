28a3aa6f131ab70952cfe722b0076665ceec272c
bdttd000 <kuba428@op.pl>
2024-04-30 20:00:25 +0200

    added types for chessboard and notation

---
+++ src/types/chessTypes.ts
@@ -0,0 +1,25 @@
+export type chessMoveType = {
+  moveNumber: number;
+  notification: string;
+  chessBoard: (null | string)[][];
+  moveInNotation: string;
+};
+
+export type chessBoardType = (string | null)[][];
+
+export type finalObjectType = chessMoveType[];
+
+export type possibleResults = "0-1" | "1-0" | "1/2-1/2";
+
+export type finalNotation = {
+  firstPiece?: string;
+  firstPosition?: string;
+  capture?: boolean;
+  secondPiece?: string;
+  secondPosition?: string;
+  check?: boolean;
+  castling?: string;
+  promotion?: boolean;
+  promotionPiece?: string;
+  checkMate?: boolean;
+};
