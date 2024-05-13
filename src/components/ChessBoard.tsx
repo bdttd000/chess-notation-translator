806cf290de8e4e886712d2d846b67e47e1d82201
bdttd000 <kuba428@op.pl>
2024-05-13 20:26:16 +0200

    added square and chessboard components

---
+++ src/components/ChessBoard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { chessBoardType } from "../types/chessTypes";
+import Square from "./Square";
+
+const ChessBoard = ({ chessBoard }: { chessBoard: chessBoardType }) => {
+  const renderBoard = () => {
+    const board = [];
+    for (let i = 0; i < 8; i++) {
+      const row = [];
+      for (let j = 0; j < 8; j++) {
+        let color = (i + j) % 2 === 0 ? "white" : "black";
+        row.push(<Square key={j} color={color} piece={chessBoard[i][j]} />);
+      }
+      board.push(
+        <div key={i} className="flex">
+          {row}
+        </div>
+      );
+    }
+    return board;
+  };
+
+  return <>{renderBoard()}</>;
+};
+
+export default ChessBoard;
